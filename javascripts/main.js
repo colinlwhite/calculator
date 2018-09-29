4eaae09936f60cdc19a815e2060cc9b8cd53861b
Colin White <clwhite814@gmail.com>
2018-09-29 11:45:54 -0500

    math all working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,11 @@
-import {printToDom} from './helpers/util.js'; 
-import {multiply} from './helpers/maths.js';
+import {calculate} from './components/calculator.js';
 
-const calculate = (num1, num2, mathType) => {
-    let answer = 0;
-    switch(mathType){
-        case 'multiply':
-            answer = multiply(num1, num2);
-            break;
-        case 'divide': 
-            answer = num1 / num2 
-            break;
-        case 'add': 
-            answer = num1 + num2;
-            break;
-        case 'subtract':
-            answer = num1-num2;
-            break;
-            default:
-            answer = 'nope';
-    }
-    printToDom(answer, 'result');
-};
+//(calculate(4, 2, 'multiply')); // 8 
 
-(calculate(4, 2, 'multiply')); // 8 
+//(calculate(4, 2, 'divide')); // 2 
+
+(calculate(44, 2, 'add')); // 46 
+
+
+
 
