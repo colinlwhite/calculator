2f953f484c4351982bcc57b3aefbdb85a12f1982
Colin White <clwhite814@gmail.com>
2018-09-29 12:34:18 -0500

    wip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,20 @@
 import {calculate} from './components/calculator.js';
+import buttonEvents from './helpers/buttonEvents.js';
 
 //(calculate(4, 2, 'multiply')); // 8 
 
 //(calculate(4, 2, 'divide')); // 2 
 
 (calculate(44, 2, 'add')); // 46 
+sevenButtonEvent();
+const initializeApp = () => {
+        buttonEvents();
+        //(calculate(4, 2, 'multiply')); // 8 
+        //(calculate(4, 2, 'divide')); // 2 
+        (calculate(44, 2, 'add')); // 46 
+};
 
+initializeApp();
 
 
 
