00729e01b735873bbde0cfaa6e619a0692148029
Colin White <clwhite814@gmail.com>
2018-09-29 13:15:14 -0500

    saving to first number working, intial display working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,9 @@
-import {calculate} from './components/calculator.js';
+import {initialDisplay} from './components/calculator.js';
 import buttonEvents from './helpers/buttonEvents.js';
 
-//(calculate(4, 2, 'multiply')); // 8 
-
-//(calculate(4, 2, 'divide')); // 2 
-
-(calculate(44, 2, 'add')); // 46 
-sevenButtonEvent();
 const initializeApp = () => {
         buttonEvents();
-        //(calculate(4, 2, 'multiply')); // 8 
-        //(calculate(4, 2, 'divide')); // 2 
-        (calculate(44, 2, 'add')); // 46 
+        initialDisplay();
 };
 
 initializeApp();
